2d41b5f2e23429d09f74da0c2939830fedcdba23
rana0045 <ranaanees26@gmail.com>
2024-03-23 12:47:21 -0700

    courses data

---
+++ utils/Data.js
@@ -0,0 +1,215 @@
+const coursesData = [
+    {
+        "id": 1,
+        "title": "Arabic Course",
+        "image": "arabic_course_image_url",
+        "content": "Description of the Arabic course...",
+        "sections": [
+            {
+                "title": "Scenario Based",
+                "scenarios": [
+                    {
+                        "id": "unique_id_1",
+                        "title": "How to Count in Arabic",
+                        "description": "Description of how to count in Arabic...",
+                        "translations": [
+                            {
+                                "english": "one",
+                                "arabic": "واحد"
+                            },
+                            {
+                                "english": "two",
+                                "arabic": "اثنان"
+                            },
+                            {
+                                "english": "three",
+                                "arabic": "ثلاثة"
+                            }
+                        ]
+                    },
+                    {
+                        "id": "unique_id_2",
+                        "title": "Daily Basic Communication in Arabic",
+                        "description": "Description of daily basic communication in Arabic...",
+                        "translations": [
+                            {
+                                "english": "hello",
+                                "arabic": "مرحبا"
+                            },
+                            {
+                                "english": "goodbye",
+                                "arabic": "وداعا"
+                            },
+                            {
+                                "english": "thank you",
+                                "arabic": "شكرا لك"
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                "title": "Video Based",
+                "videos": [
+                    {
+                        "id": "unique_id_3",
+                        "title": "Arabic Video Lesson 1",
+                        "url": "https://www.youtube.com/watch?v=6OE064HClCE&embeds_referring_euri=https%3A%2F%2Fapp.memrise.com%2F&embeds_referring_origin=https%3A%2F%2Fapp.memrise.com&source_ve_path=Mjg2NjY&feature=emb_logo",
+                        "image": "video_thumbnail_url",
+                        "tracking_id": "tracking_id_1"
+                    },
+                    {
+                        "id": "unique_id_4",
+                        "title": "Arabic Video Lesson 2",
+                        "url": "https://www.youtube.com/watch?v=0kDjspnzHgQ",
+                        "image": "video_thumbnail_url",
+                        "tracking_id": "tracking_id_2"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "id": 2,
+        "title": "English Course",
+        "image": "english_course_image_url",
+        "content": "Description of the English course...",
+        "sections": [
+            {
+                "title": "Scenario Based",
+                "scenarios": [
+                    {
+                        "id": "unique_id_5",
+                        "title": "Daily Conversation in English",
+                        "description": "Description of daily conversation in English...",
+                        "translations": [
+                            {
+                                "english": "hello",
+                                "arabic": "مرحبا"
+                            },
+                            {
+                                "english": "goodbye",
+                                "arabic": "وداعا"
+                            },
+                            {
+                                "english": "thank you",
+                                "arabic": "شكرا لك"
+                            }
+                        ]
+                    },
+                    {
+                        "id": "unique_id_6",
+                        "title": "Writing Emails in English",
+                        "description": "Description of writing emails in English...",
+                        "translations": [
+                            {
+                                "english": "email",
+                                "arabic": "بريد إلكتروني"
+                            },
+                            {
+                                "english": "subject",
+                                "arabic": "موضوع"
+                            },
+                            {
+                                "english": "attachment",
+                                "arabic": "مرفق"
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                "title": "Video Based",
+                "videos": [
+                    {
+                        "id": "unique_id_7",
+                        "title": "English Grammar Lesson 1",
+                        "url": "https://www.youtube.com/watch?v=XDCuYcxdG8E",
+                        "image": "video_thumbnail_url",
+                        "tracking_id": "tracking_id_3"
+                    },
+                    {
+                        "id": "unique_id_8",
+                        "title": "English Pronunciation Lesson 1",
+                        "url": "https://www.youtube.com/watch?v=lAk6I94IZ_E",
+                        "image": "video_thumbnail_url",
+                        "tracking_id": "tracking_id_4"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "id": 3,
+        "title": "Spanish Course",
+        "image": "spanish_course_image_url",
+        "content": "Description of the Spanish course...",
+        "sections": [
+            {
+                "title": "Scenario Based",
+                "scenarios": [
+                    {
+                        "id": "unique_id_9",
+                        "title": "Introducing Yourself in Spanish",
+                        "description": "Description of how to introduce yourself in Spanish...",
+                        "translations": [
+                            {
+                                "english": "hello",
+                                "spanish": "hola"
+                            },
+                            {
+                                "english": "goodbye",
+                                "spanish": "adiós"
+                            },
+                            {
+                                "english": "thank you",
+                                "spanish": "gracias"
+                            }
+                        ]
+                    },
+                    {
+                        "id": "unique_id_10",
+                        "title": "Ordering Food in Spanish",
+                        "description": "Description of ordering food in Spanish...",
+                        "translations": [
+                            {
+                                "english": "menu",
+                                "spanish": "menú"
+                            },
+                            {
+                                "english": "water",
+                                "spanish": "agua"
+                            },
+                            {
+                                "english": "bill",
+                                "spanish": "cuenta"
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                "title": "Video Based",
+                "videos": [
+                    {
+                        "id": "unique_id_11",
+                        "title": "Spanish Grammar Lesson 1",
+                        "url": "https://www.youtube.com/watch?v=6OE064HClCE&embeds_referring_euri=https%3A%2F%2Fapp.memrise.com%2F&embeds_referring_origin=https%3A%2F%2Fapp.memrise.com&source_ve_path=Mjg2NjY&feature=emb_logo",
+                        "image": "video_thumbnail_url",
+                        "tracking_id": "tracking_id_5"
+                    },
+                    {
+                        "id": "unique_id_12",
+                        "title": "Spanish Pronunciation Lesson 1",
+                        "url": "https://www.youtube.com/watch?v=0kDjspnzHgQ",
+                        "image": "video_thumbnail_url",
+                        "tracking_id": "tracking_id_6"
+                    }
+                ]
+            }
+        ]
+    },
+]
+
+
+export default coursesData;
