c89d8ed1d8b657e21b6d35ce5fc897956318261b
Haris Shaikh <harisahmedsheikh1997@gmail.com>
2024-04-01 04:53:29 +0500

    kjhksjdfhgdfds

--- app/home/page.js
+++ app/home/page.js
@@ -5,18 +5,23 @@ import Layout from "@/components/layout/Layout"
 // import DiscountBanner from "@/components/discount-banner/DiscountBanner"
 import WeeklyProgressCard from "@/components/weekly-progress-card/WeeklyProgressCard"
 import WeeklyProgressCardGrid from "@/components/weekly-progress-card-grid/WeeklyProgreeCardGrid"
+import { ScenarioCard } from "@/components/scenario-card/ScenarioCard"
+import coursesData from "@/utils/Data"
 
 export default function Home() {
 	const [selectedTab, setSelectedTab] = useState("green")
 	const [showCards, setShowCards] = useState(false)
+    const [showPopup, setShowPopup] = useState(false);
+    const [selectedScenario, setSelectedScenario] = useState();
 
 	useEffect(() => {
 		setShowCards(true)
 	}, [])
 	return (
 		<Layout>
-			<div className="min-h-full flex-1 flex flex-col items-center">
+			<div className="min-h-full flex-1 flex flex-col items-center relative">
 				{/* <DiscountBanner /> */}
+				{showPopup && <ScenarioPopup id={selectedScenario} setShowPopup={setShowPopup} />}
 				<div
 					className="h-full w-full flex-1 flex flex-col gap-5 items-center bg-cover bg-center"
 					style={{
@@ -44,6 +49,45 @@ export default function Home() {
 						selectedTab={selectedTab}
 						setSelectedTab={setSelectedTab}
 					/>}
+					<div className="w-full flex flex-col gap-5 pl-10 mb-10">
+                        <p className="font-bold">Recommended by Teacher</p>
+                        <div className="flex flex-row items-center gap-5 w-full">
+                            {/* {isLeftButtonVisible && (
+                                <button onClick={scrollToLeft}>
+                                    <ChevronLeftIcon className="size-5 text-black" />
+                                </button>
+                            )} */}
+                            <div
+                                className="w-full overflow-x-auto scrollbar-none"
+                            >
+                                <div className="flex flex-col sm:flex-row gap-5 sm:items-center">
+                                    {coursesData
+                                        ?.find((course) => {
+                                            return (
+                                                course.id === 1
+                                            );
+                                        })
+                                        ?.sections[0]?.scenarios?.map(
+                                            (item, key) => (
+                                                <ScenarioCard
+													id={item?.id}
+                                                    name={item?.title}
+                                                    isPremium={item?.isPremium}
+													setSelectedScenario={setSelectedScenario}
+													setShowPopup={setShowPopup}
+                                                    key={key}
+                                                />
+                                            )
+                                        )}
+                                </div>
+                            </div>
+                            {/* {isRightButtonVisible && (
+                                <button onClick={scrollToRight}>
+                                    <ChevronRightIcon className="size-5 text-black" />
+                                </button>
+                            )} */}
+                        </div>
+                    </div>
 				</div>
 			</div>
 		</Layout>
