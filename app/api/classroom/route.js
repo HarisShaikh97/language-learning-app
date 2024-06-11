85a5fc9f24d9367bf0b04271860a1765133cd8c3
HarisShaikh97 <harisahmedsheikh1997@gmail.com>
2024-06-12 03:21:37 +0500

    fhdfgh

--- app/api/classroom/route.js
+++ app/api/classroom/route.js
@@ -1,88 +1,128 @@
-import Classroom from "@/app/models/classroom.model";
-import User from "@/app/models/user.model";
-import connect from "@/app/db/connect";
-import { NextResponse } from "next/server";
+import Classroom from "@/app/models/classroom.model"
+import User from "@/app/models/user.model"
+import connect from "@/app/db/connect"
+import { NextResponse } from "next/server"
 
 export async function POST(req) {
+	try {
+		connect()
+		const reqBody = await req.json()
+		const { name, description, teacher, students, assignments } = reqBody
 
-    try {
-        connect()
-        const reqBody = await req.json()
-        const { name, description, teacher, students, assignments } = reqBody
+		const isTeacher = await User.findOne({ _id: teacher, role: "teacher" })
+		const isStudents = await User.find({
+			_id: { $in: students },
+			role: "student"
+		})
 
-        const isTeacher = await User.findOne({ _id: teacher, role: "teacher" })
-        const isStudents = await User.find({ _id: { $in: students }, role: "student" })
+		if (!isTeacher) {
+			return NextResponse.json(
+				{ error: "Invalid teacher" },
+				{ status: 400 }
+			)
+		}
 
-        if (!isTeacher) {
-            return NextResponse.json({ error: "Invalid teacher" }, { status: 400 })
-        }
+		if (isStudents.length !== students.length) {
+			return NextResponse.json(
+				{ error: "One or more students  IDs are invalid" },
+				{ status: 400 }
+			)
+		}
 
-        if (isStudents.length !== students.length) {
-            return NextResponse.json({ error: 'One or more students  IDs are invalid' }, { status: 400 });
+		const isClass = await Classroom.find({ name })
 
-        }
+		if (isClass.length > 0) {
+			return NextResponse.json(
+				{ error: "Classroom already exists" },
+				{ status: 400 }
+			)
+		}
+		const newClass = new Classroom({
+			name,
+			description,
+			teacher: teacher,
+			students: students,
+			assignments: assignments
+		})
 
-        const isClass = await Classroom.find({ name })
-
-        if (isClass.length > 0) {
-            return NextResponse.json({ error: "Classroom already exists" }, { status: 400 })
-        }
-        const newClass = new Classroom({
-            name,
-            description,
-            teacher: teacher,
-            students: students,
-            assignments: assignments
-        })
-
-        const Data = await newClass.save()
-        return NextResponse.json({ message: "Classroom created successfully", data: Data, succuss: true }, { status: 201 })
-
-    } catch (error) {
-        return NextResponse.json({ error: error.message }, { status: 500 })
-    }
+		const Data = await newClass.save()
+		return NextResponse.json(
+			{
+				message: "Classroom created successfully",
+				data: Data,
+				succuss: true
+			},
+			{ status: 201 }
+		)
+	} catch (error) {
+		return NextResponse.json({ error: error.message }, { status: 500 })
+	}
 }
 
 export async function GET(req) {
-    try {
-        connect()
+	try {
+		connect()
 
-        const id = await req.nextUrl.searchParams.get("id")
-        if (!id) {
-            const allClassrooms = await Classroom.find().populate("teacher").populate("students")
-            return NextResponse.json({ message: "All classrooms", Data: allClassrooms, success: true }, { status: 200 })
-        }
+		const id = await req.nextUrl.searchParams.get("id")
+		if (!id) {
+			const allClassrooms = await Classroom.find()
+				.populate("teacher")
+				.populate("students")
+			return NextResponse.json(
+				{
+					message: "All classrooms",
+					Data: allClassrooms,
+					success: true
+				},
+				{ status: 200 }
+			)
+		}
 
-        const classroom = await Classroom.findById(id).populate("teacher").populate("students")
+		const classroom = await Classroom.findById(id)
+			.populate("teacher")
+			.populate("students")
 
-        if (!classroom) {
-            return NextResponse.json({ message: "Classroom Not found", success: true }, { status: 404 })
-        }
+		if (!classroom) {
+			return NextResponse.json(
+				{ message: "Classroom Not found", success: true },
+				{ status: 404 }
+			)
+		}
 
-        return NextResponse.json({ message: "Classroom found", Data: classroom, success: true }, { status: 200 })
-
-    } catch (error) {
-        return NextResponse.json({ error: error.message }, { status: 500 })
-    }
+		return NextResponse.json(
+			{ message: "Classroom found", Data: classroom, success: true },
+			{ status: 200 }
+		)
+	} catch (error) {
+		return NextResponse.json({ error: error.message }, { status: 500 })
+	}
 }
 
 export async function DELETE(req) {
-    try {
+	try {
+		connect()
+		const id = await req.nextUrl.searchParams.get("id")
 
-        connect()
-        const id = await req.nextUrl.searchParams.get("id")
+		if (!id) {
+			return NextResponse.json(
+				{ error: "Invalid classroom ID" },
+				{ status: 400 }
+			)
+		}
 
-        if (!id) {
-            return NextResponse.json({ error: "Invalid classroom ID" }, { status: 400 })
-        }
+		const DeletedClass = await Classroom.findByIdAndDelete(id)
+		if (!DeletedClass) {
+			return NextResponse.json(
+				{ error: "Classroom not found" },
+				{ status: 404 }
+			)
+		}
 
-        const DeletedClass = await Classroom.findByIdAndDelete(id)
-        if (!DeletedClass) {
-            return NextResponse.json({ error: "Classroom not found" }, { status: 404 })
-        }
-
-        return NextResponse.json({ message: "Classroom deleted successfully", success: true }, { status: 200 })
-    } catch (error) {
-        return NextResponse.json({ error: error.message }, { status: 500 })
-    }
+		return NextResponse.json(
+			{ message: "Classroom deleted successfully", success: true },
+			{ status: 200 }
+		)
+	} catch (error) {
+		return NextResponse.json({ error: error.message }, { status: 500 })
+	}
 }
